b369d0009edf658240e21a856e04d406b22433c4
Chris Collins <29593332+christides11@users.noreply.github.com>
2023-07-31 12:39:32 -0700

    feat: Equals for item references.

--- DataTypes/ModStringContentItemReference.cs
+++ DataTypes/ModStringContentItemReference.cs
@@ -1,9 +1,18 @@
+using System;
+
 namespace mcf
 {
     [System.Serializable]
-    public struct ModStringContentItemReference
+    public struct ModStringContentItemReference : IEquatable<ModStringContentSetItemReference>
     {
         public ModStringContentReference contentReference;
         public string ItemReference;
+
+        public bool Equals(ModStringContentSetItemReference other)
+        {
+            return contentReference.modGUID == other.contentReference.modGUID
+                && contentReference.contentGUID == other.contentReference.contentGUID
+                && ItemReference == other.ItemReference;
+        }
     }
 }
